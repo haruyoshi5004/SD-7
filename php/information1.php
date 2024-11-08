a523fd8681eaeb52e85bb804f4bdf1112616d7cf
mot140 <c3p31108@bunkyo.ac.jp>
2024-11-08 14:18:46 +0900

    aa

--- php/information1.php
+++ php/information1.php
@@ -1,12 +1,12 @@
 <?php
 if(isset($_POST['user'],$_POST['pass'])){
-a = $_POST["user"];
+$a = $_POST["user"];
 $arr = array($_POST["pass"]);
 $dsn = "mysql:dbname=sinadasi;host=localhost";
 $my = new PDO($dsn, "sina", "sina");
 $sql = "SELECT パスワード WHERE ユーザー名 = a FROM ログイン管理;";
 $st = $my -> query($sql);
-if($st == a){
+if($st == $a){
     header("Location:information2.html");
 }else{
     $alert = "<script type='text/javascript'>alert('これはalertです。');</script>";
