1f5f32109d36295f268d654b0d7f83d331360a45
mot140 <c3p31108@bunkyo.ac.jp>
2024-11-11 23:24:06 +0900

    aaa

--- php/information14.php
+++ php/information14.php
@@ -1,17 +1,35 @@
 <?php
 if (isset($_POST["name"], $_POST["kengen"],$_POST["user"])) {
     $a = $_POST["name"];  // ユーザー名
-    $password = $_POST["kengen"];  // パスワード
+    $ken = $_POST["kengen"];  // パスワード
     $user = $_POST["user"];
     $dsn = "mysql:dbname=shinadasi;host=localhost";
+    try{
+        $my = new PDO($dsn, "sina", "sina");
+        // SQL文（パスワードを取得）
+        $sql = "SELECT 管理者id FROM ログイン管理 WHERE ユーザー名 = :user";
+        // SQL準備
+        $st = $my->prepare($sql);
+        $st->bindParam(':user', $a, PDO::PARAM_STR);
+        $st->execute();
+        // 結果を取得
+        $result = $st->fetch(PDO::FETCH_ASSOC);
 
-    $my = new PDO($dsn, "sina", "sina");
-    // SQL文（パスワードを取得）
-    $sql = "SELECT 管理者id FROM ログイン管理 WHERE ユーザー名 = :user";
-    // SQL準備
-    $st = $my->prepare($sql);
-    $st->bindParam(':user', $a, PDO::PARAM_STR);
-    $st->execute();
+        // SQL文（パスワードを取得）
+        $sql = "INSERT INTO ユーザー名(管理者id,名前,管理者権限) VALUES (:id,:user,:ken)";
+        
+        // SQL準備
+        $st = $my->prepare($sql);
+        $params = array(':id' => $result, ':user' => $name,":ken" => $ken);
 
-    // 結果を取得
-    $result = $st->fetch(PDO::FETCH_ASSOC);
+        if ($st->execute($params)) {
+            echo "挿入に成功しました！";
+        } else {
+            echo "挿入に失敗しました！";
+        }
+
+    } catch (PDOException $e) {
+        echo "接続または操作に失敗しました: " . $e->getMessage();
+    }
+}
+?>
