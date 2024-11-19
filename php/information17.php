ebaebe3419b4259a44fdc5b27c0cd34e641b2d60
mot140 <c3p31108@bunkyo.ac.jp>
2024-11-19 10:40:27 +0900

    aaa

--- php/information17.php
+++ php/information17.php
@@ -1,15 +1,18 @@
 <?php
 if(isset($_POST["user"])){
-    $username =$_POST["user"];
-    $dsn = "mysql:dbname=shinadasi;host=localhost";
-    $my = new PDO($dsn,"sina","sina");
-    $sql = "SELECT 管理者ID FROM ユーザー管理 WHERE 名前 = :user ";
-    $st = $my->prepare($sql);
-    $st->bindParam(':user', $username, PDO::PARAM_STR);
-    $st->execute();
-    $result = $st->fetch(PDO::FETCH_ASSOC);
-    $adomin_id = $result["管理者ID"];
-    $sql="DELETE FROM ユーザー管理 WHERE 名前 = $username";
-    $st =$my -> prepare($sql);
-    
+    try{
+        $username =$_POST["user"];
+        $dsn = "mysql:dbname=shinadasi;host=localhost";
+        $my = new PDO($dsn,"sina","sina");
+        $sql = "SELECT 管理者ID FROM ユーザー名 WHERE 名前 = :user ";
+        $st = $my->prepare($sql);
+        $st->bindParam(':user', $username, PDO::PARAM_STR);
+        $st->execute();
+        $result = $st->fetch(PDO::FETCH_ASSOC);
+        $adomin_id = $result["管理者ID"];
+        $sql="DELETE FROM ユーザー管理 WHERE 名前 = $username";
+        $st =$my -> prepare($sql);
+        $st->execute();
+        $sql = "DELETE FROM ログイン管理 WHERE "
+    }
 }
