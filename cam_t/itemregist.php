4c1ef51b505cb34debc5dd16d07822526ae96117
TaaKoooB4 <c3p31037@bunkyo.ac.jp>
2024-11-25 22:24:03 +0900

    情報登録8(仮)

---
+++ cam_t/itemregist.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>スーパーの管理項目</title>
+    <link rel="stylesheet" href="情報登録8.css">
+</head>
+<body>
+    <header class="header">
+        情報登録
+    </header>
