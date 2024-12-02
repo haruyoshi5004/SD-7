0c995bf46fe25947cd1c0856df2f6d92f64407f9
mot140 <c3p31108@bunkyo.ac.jp>
2024-12-02 15:23:12 +0900

    aaa

---
+++ cam_k/template/品出し1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html lang ="ja">
+    <head>
+        <meta charset ="utf-8">
+        <title>商品一覧</title>
+    </head>
+    <body>
+        <?php
+        $dsn = "mysql:dbname=shinadasi;host=localhost";
+        try{
+            $my = new PDO($dsn, "sina", "sina");
+            $my->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "SELECT 棚番号 FROM 棚";
+            $st = $my->prepare($sql);
+            $st->execute();
+            $result = $st->fetch(PDO::FETCH_ASSOC);
+            echo "<table border =1>";
+            foreach($result as $row){
+                echo "<tr><td>";
+                echo "棚".$row['棚番号'];
+                echo "</td>";
+                echo "<td>";
+                echo "<form action ='syoken.php' method ='post'>";
+                echo "<input type='hidden' name='tana' value='" . htmlspecialchars($row['棚番号']) . "'>";
+                echo "<input type = 'submit' value='詳細'>";
+                echo "</form>";
+                echo "</td></tr>";
+            }
+            echo "</table>";
+        } catch (PDOException $e) {
+            echo "エラー: " . htmlspecialchars($e->getMessage());
+        }
+        ?>
+        </body>
+    </html>
