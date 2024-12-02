e552d43d1a2e5efb8dd5c99aa5348fbe5c90f846
mot140 <c3p31108@bunkyo.ac.jp>
2024-12-03 00:48:44 +0900

    aaa

--- cam_k/template/syoken.php
+++ cam_k/template/syoken.php
@@ -4,15 +4,16 @@ if(isset($_POST["tana"])) {
     $dsn = "mysql:dbname=shinadasi;host=localhost";
     try {
         $my = new PDO($dsn, "sina", "sina");
-        $sql = "SELECT 商品ID FROM 棚 WHERE 棚番号 = $tana ORDER BY 商品ID ASC";
+        $sql = "SELECT 商品ID FROM 棚 WHERE 棚番号 = :tana ORDER BY 商品ID ASC";
         $st = $my->prepare($sql);
+        $st->bindParam(':tana', $tana, PDO::PARAM_INT);
         $st->execute();
         $result = $st->fetchAll(PDO::FETCH_ASSOC);
 
-        $syo =[];
-        foreach($result as $row){
+        $syo = [];
+        foreach($result as $row) {
             $re = $row['商品ID'];
-            $sql ="SELECT 商品名 FROM 商品 WHERE 商品ID= :re";
+            $sql = "SELECT 商品名 FROM 商品 WHERE 商品ID= :re";
             $st = $my->prepare($sql);
             $st->bindParam(':re', $re, PDO::PARAM_INT);
             $st->execute();
@@ -21,9 +22,10 @@ if(isset($_POST["tana"])) {
         }
         $syo_json = json_encode($syo);
         $syo_encoded = urlencode($syo_json);
-        header('Location:品出し2.html?syo=$syo_encoded');
-        }catch (PDOException $e){
-            echo "エラー: " . $e->getMessage();
-        }
+        $tana_encoded = urlencode($tana);
+        header("Location:../../Stocking/品出し2.html?syo=$syo_encoded&shelf=$tana_encoded");
+    } catch (PDOException $e) {
+        echo "エラー: " . $e->getMessage();
     }
-?> 
+}
+?>
