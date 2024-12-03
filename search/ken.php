a9909a8132825cef29fec043357e6e49e4149294
mot140 <c3p31108@bunkyo.ac.jp>
2024-12-03 11:10:59 +0900

    aaa

--- search/ken.php
+++ search/ken.php
@@ -21,33 +21,40 @@ if (isset($_POST["s"], $_POST["syo"], $_POST["category"])) {
             $st = $my->prepare($sql);
             $st->bindParam(':id', $cate_id, PDO::PARAM_INT);
             $st->execute();
-            $result = $st->fetch(PDO::FETCH_ASSOC);
-            $syo_id = $result["商品ID"];
+            $result = $st->fetchAll(PDO::FETCH_ASSOC);
 
-            $sql = "SELECT 商品名 FROM 商品 WHERE 商品ID = :id";
-            $st = $my->prepare($sql);
-            $st->bindParam(':id', $syo_id, PDO::PARAM_INT);
-            $st->execute();
-            $result = $st->fetch(PDO::FETCH_ASSOC);
-            foreach($result as $row){
-                
+            $products = [];
+            foreach ($result as $row) {
+                $syo_id = $row["商品ID"];
+                $sql = "SELECT 商品名 FROM 商品 WHERE 商品ID = :id";
+                $st = $my->prepare($sql);
+                $st->bindParam(':id', $syo_id, PDO::PARAM_INT);
+                $st->execute();
+                $product = $st->fetch(PDO::FETCH_ASSOC);
+                $products[] = $product["商品名"];
             }
+            $products_encoded = urlencode(json_encode($products));
+            header("Location:検索結果2.html?syo=$products_encoded");
         } elseif ($syo == "" && $cate == "") {
             $sql = "SELECT 商品名 FROM 商品 WHERE Janコード = :jan";
             $st = $my->prepare($sql);
             $st->bindParam(':jan', $jan, PDO::PARAM_STR);
             $st->execute();
             $result = $st->fetch(PDO::FETCH_ASSOC);
-            echo htmlspecialchars($result["商品名"]);
+            $product_encoded = urlencode($result["商品名"]);
+            header("Location:検索結果3.html?syo=$product_encoded");
         } elseif ($jan == "" && $cate == "") {
-            $sql = "SELECT 商品名 FROM 商品 WHERE 商品名 = :syo";
+            $sql = "SELECT 商品名 FROM 商品 WHERE 商品名 Like :syo";
             $st = $my->prepare($sql);
             $st->bindParam(':syo', $syo, PDO::PARAM_STR);
             $st->execute();
             $result = $st->fetchAll(PDO::FETCH_ASSOC);
+            $products = [];
             foreach ($result as $row) {
-                echo htmlspecialchars($row["商品名"]);
+                $products[] = $row["商品名"];
             }
+            $products_encoded = urlencode(json_encode($products));
+            header("Location:検索結果1.html?syo=$products_encoded");
         } else {
             echo "<script type='text/javascript'>
             alert('入力エラーです。');
